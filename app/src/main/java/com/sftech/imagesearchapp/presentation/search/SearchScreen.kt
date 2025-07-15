c908f849fa6d2251d641e7f6e5eba102aefb6ee2
mzafar <mzafar@creativechaos.co>
2025-07-15 19:11:23 +0500

    Chore: Improve Image Loading along with Image rendering on the screen.

--- app/src/main/java/com/sftech/imagesearchapp/presentation/search/SearchScreen.kt
+++ app/src/main/java/com/sftech/imagesearchapp/presentation/search/SearchScreen.kt
@@ -1,9 +1,9 @@
 package com.sftech.imagesearchapp.presentation.search
 
-import android.widget.Toast
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,11 +19,14 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -37,7 +40,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.request.CachePolicy
 import coil.request.ImageRequest
+import coil.size.Size
 import com.sftech.imagesearchapp.R
 import com.sftech.imagesearchapp.domain.model.ImageItem
 
@@ -52,7 +58,7 @@ fun SearchScreen(
 
         val query: MutableState<String> = remember { mutableStateOf("") }
         val gridState = rememberLazyGridState()
-        val state = viewModel.list.value
+        val state by viewModel.state.collectAsState()
 
         Column(
             modifier = Modifier
@@ -61,11 +67,11 @@ fun SearchScreen(
                 .padding(8.dp)
         ) {
             OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().padding(4.dp),
                 value = query.value,
                 onValueChange = { it ->
                     query.value = it
-                    viewModel.getImageList(query.value)
+                    viewModel.onSearchQueryChanged(it)
                 },
                 enabled = true,
                 singleLine = true,
@@ -75,37 +81,93 @@ fun SearchScreen(
 
             Spacer(modifier = Modifier.height(8.dp))
 
-            if (state.isLoading) {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-                }
-            }
-            if (state.error.isNotBlank()) {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    Text(
-                        text = viewModel.list.value.error,
-                        fontSize = 20.sp,
-                        modifier = Modifier.align(Alignment.Center)
-                    )
-                }
-            }
-
-            if (state.data.isNotEmpty()) {
-
-                LazyVerticalGrid(
-                    state = gridState,
-                    columns = GridCells.Fixed(2)
-                ) {
-                    viewModel.list.value.data.let {
-                        items(it, key = { it.id }) {
-                            ImageContainer(it)
-                        }
+            when (val currentState = state) {
+                is SearchScreenViewModel.State.Idle -> {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            "Enter a search term to begin",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
                     }
-
                 }
 
+                is SearchScreenViewModel.State.Error -> {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Text(
+                            text = currentState.message,
+                            fontSize = 20.sp,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                }
 
+                SearchScreenViewModel.State.Loading -> {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                    }
+                }
+
+                is SearchScreenViewModel.State.Success -> {
+                    LazyVerticalGrid(
+                        state = gridState,
+                        columns = GridCells.Fixed(2),
+                        contentPadding = PaddingValues(8.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        items(currentState.images, key = { it.id }) {
+                            ImageContainer(
+                                imageItem = it,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(200.dp)
+                                    .padding(4.dp)
+                            )
+                        }
+
+                    }
+                }
             }
+            /*  if (state.isLoading) {
+                  Box(modifier = Modifier.fillMaxSize()) {
+                      CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                  }
+              }
+              if (state.error.isNotBlank()) {
+                  Box(modifier = Modifier.fillMaxSize()) {
+                      Text(
+                          text = state.error,
+                          fontSize = 20.sp,
+                          modifier = Modifier.align(Alignment.Center)
+                      )
+                  }
+              }
+
+              if (state.data.isNotEmpty()) {
+
+                  LazyVerticalGrid(
+                      state = gridState,
+                      columns = GridCells.Adaptive(minSize = 150.dp),
+                      contentPadding = PaddingValues(8.dp),
+                      verticalArrangement = Arrangement.spacedBy(8.dp),
+                      horizontalArrangement = Arrangement.spacedBy(8.dp)
+                  ) {
+                      items(state.data, key = { it.id }) {
+                          ImageContainer(
+                              imageItem = it,
+                              modifier = Modifier
+                                  .fillMaxWidth()
+                                  .height(200.dp)
+                          )
+                      }
+
+                  }
+
+
+              }*/
 
         }
     }
@@ -117,21 +179,26 @@ fun SearchScreen(
 fun ImageContainer(imageItem: ImageItem, modifier: Modifier = Modifier) {
 
     Card(
-        modifier = modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-            .height(200.dp),
+        modifier = modifier,
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
         AsyncImage(
             model = ImageRequest.Builder(context = LocalContext.current)
                 .data(imageItem.imageUrl)
+                .diskCachePolicy(CachePolicy.ENABLED)
+                .memoryCachePolicy(CachePolicy.ENABLED)
+                .placeholderMemoryCacheKey(imageItem.previewImageUrl)
                 .crossfade(true)
                 .build(),
             contentDescription = stringResource(R.string.searched_image),
             modifier = Modifier.fillMaxWidth(),
             error = painterResource(R.drawable.ic_broken_image),
-            placeholder = painterResource(R.drawable.loading_img),
+            placeholder = rememberAsyncImagePainter(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageItem.previewImageUrl)
+                    .size(Size.ORIGINAL)
+                    .build()
+            ),
             contentScale = ContentScale.Crop,
         )
     }
--- app/src/main/java/com/sftech/imagesearchapp/presentation/search/SearchScreenViewModel.kt
+++ app/src/main/java/com/sftech/imagesearchapp/presentation/search/SearchScreenViewModel.kt
@@ -1,19 +1,26 @@
 package com.sftech.imagesearchapp.presentation.search
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sftech.imagesearchapp.domain.model.ImageItem
 import com.sftech.imagesearchapp.domain.use_case.GetImageUseCase
-import com.sftech.imagesearchapp.util.MainState
 import com.sftech.imagesearchapp.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.milliseconds
 
 @HiltViewModel
 class SearchScreenViewModel @Inject constructor(
@@ -21,41 +28,118 @@ class SearchScreenViewModel @Inject constructor(
 ) : ViewModel() {
 
 
-    val list: MutableState<MainState> = mutableStateOf(MainState())
+    private val _state = MutableStateFlow<State>(State.Idle)
+    val state: StateFlow<State> = _state.asStateFlow()
 
+    // Search query flow with debouncing
+    private val _searchQuery = MutableStateFlow("")
     private var searchJob: Job? = null
 
-
     init {
-        getImageList("")
+        getImageList()
+        triggerInitialLoad()
     }
 
-    fun getImageList(query: String) {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun getImageList() {
 
-        searchJob?.cancel()
+//        searchJob?.cancel()
+//
+//        searchJob = viewModelScope.launch {
+//            // Only debounce if it's a user search (not initial load)
+//            if (query.isNotBlank()) {
+//                delay(500L)
+//            }
+//
+//            getImageUseCase.invoke(query).collect { it ->
+//                when (it) {
+//                    is Resource.Loading -> {
+//                        _list.value = list.value.copy(isLoading = true)
+//                    }
+//
+//                    is Resource.Error -> {
+//                        _list.value = list.value.copy(
+//                            isLoading = false,
+//                            error = it.message.toString()
+//                        )
+//                    }
+//
+//                    is Resource.Success -> {
+//                        it.data?.let { item ->
+//                            _list.value = list.value.copy(
+//                                isLoading = false,
+//                                data = item,
+//                                error = ""
+//                            )
+//                        }
+//                    }
+//                }
+//            }
+//        }
 
         searchJob = viewModelScope.launch {
+            @OptIn(FlowPreview::class) _searchQuery.onStart { emit("") }.distinctUntilChanged()
+                .debounce(500.milliseconds).flatMapLatest { query ->
+                    _state.value = State.Loading
+                    getImageUseCase.invoke(query).map { resources ->
+                        when (resources) {
+                            is Resource.Success -> {
+                                resources.data?.let { image ->
+                                    if (image.isEmpty()) {
+                                        State.Error("No images found")
+                                    } else {
+                                        State.Success(image)
+                                    }
+                                } ?: State.Error("Empty response")
+                            }
 
-            delay(500L)
+                            is Resource.Error -> {
+                                State.Error(resources.message ?: "Unknown Error")
+                            }
 
-            getImageUseCase.invoke(query).collect { it ->
-                when (it) {
-                    is Resource.Loading -> {
-                        list.value = MainState(isLoading = true)
-                    }
-
-                    is Resource.Error -> {
-                        list.value = MainState(error = it.message.toString())
-                    }
-
-                    is Resource.Success -> {
-                        it.data?.let { item ->
-                            list.value = MainState(data = item)
+                            is Resource.Loading -> {
+                                State.Loading
+                            }
                         }
+                    }.catch { e ->
+                        emit(State.Error(e.message ?: "Unknown error"))
                     }
+                }.collect { newState ->
+                    _state.value = newState
                 }
-            }
         }
+
+
+    }
+
+    private fun triggerInitialLoad() {
+        _searchQuery.value = "" // Triggers initial load
+    }
+
+    fun onSearchQueryChanged(query: String) {
+        _searchQuery.value = query
+    }
+
+    fun retry() {
+        onSearchQueryChanged(_searchQuery.value)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        searchJob?.cancel()
+    }
+
+
+
+    // State definition
+    sealed interface State {
+        data object Idle : State
+        data object Loading : State
+        data class Success(val images: List<ImageItem>) : State
+        data class Error(val message: String) : State
     }
 
 }
+
+
+
